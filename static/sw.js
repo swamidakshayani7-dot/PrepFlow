951898552402c6a7a66c55a9283a65666333eeee
swamidakshayani7-dot <swamidakshayani7@gmail.com>
2026-04-14 20:08:36 +0530

    final pwa fix

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,10 @@
 self.addEventListener('install', function(event) {
   console.log('Service Worker Installed');
 });
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
 
 self.addEventListener('fetch', function(event) {
   // just pass through
